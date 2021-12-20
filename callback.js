2ec535b55e4c730e5ca78b708a5553823a13e6b9
Nguyen Trang <trang.nt172@gmail.com>
2021-12-21 00:56:30 +0700

    callback-es6

--- callback.js
+++ callback.js
@@ -1,15 +1,19 @@
-let a = 10;
+//callback là 1 hàm 
+//đc truyền qua đối số của 1 function khác
+//và đc gọi lại trong hàm nhận đối số
 
-function test() {
+//ở đây test2 là callback, nó đc truyền qua đối số của function test đc khai báo đầu tiên
+//test2 đc truyền làm đối số khi gọi function test thì callback chính là test2 và để chạy function test2 thì gọi nó test2() cũng chính là gọi callback()
+function test(callback) {
   setTimeout(function () {
-    console.log('running in setTimeout')
+    console.log('running in setTimeout');
+    callback()
   })
 }
 
 function test2() {
   console.log('function 2');
 }
-
-console.log(a);
-test();
-test2();
+test(test2);
+//running in setTimeout
+//function 2
