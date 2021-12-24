86e1964a999b81a4ff646a9010dafb56537a8ac3
Nguyen Trang <trang.nt172@gmail.com>
2021-12-24 14:46:30 +0700

    fix-usestate

--- useState.js
+++ useState.js
@@ -26,26 +26,4 @@ function Component() {
 //side effect sẽ gọi API lấy dữ liệu, tương tác với DOM, remove listener, setTimeout, setInterval
 // useEffect nhận 2 tham số truyền vào là callback(bắt buộc) và 1 mảng chứ những cái phụ thuộc vào dl [deps] (k bắt buộc)
 //các code logic viết trong callback để thực hiện các side effect.
-//callback luôn dc gọi sau khi component mounted ( e chưa hiểu component mounted  )
-// e mới tìm hiểu đến đây ạ
-
-//3. React.memo(): higher order component (HOC) k phải là hooks => giúp component tránh re-render trong nhiều trg hợp k cần thiết => Chỉ renderlại component nếu props thay đổi.
-
-//Memoization tính toán và lưu kết quả cho từng bộ input.
-// Khi gặp lại bộ input đã từng làm thì không tính toán lại, mà trả về kết quả sẵn có.
-// Dùng cho các xử lý, tính toán nặng, tốn thời gian, tài nguyên.
-
-//props: truyền dl từ component cha xuống component con
-
-//4. useCallback sử dụng để tối ưu quá trình render của React functional components => giúp component tránh re-render trong nhiều trg hợp k cần thiết => tránh hàm không được tạo lại lần nữa, dựa trên mảng các phần phụ thuộc. Trả về chính function đó.
-// usecallback nhận vào 2 tham số: 1 là function, 2 là dependencies(mảng phụ thuộc)
-// Return memoized callback
-// chỉ tạo ra function mới khi dependencies thay đổi.
-// nếu dùng empty dependencies thì không bao giờ tạo ra function mới.
-
-//5. useMemo giữ cho một hàm không được thực thi lại nếu nó không nhận được một tập hợp các tham số đã được sử dụng trước
-//Nhận vào 2 tham số: 1 là function, 2 là dependencies.
-// Return memoized value
-// Chỉ tính toán value mới khi dependencies thay đổi.
-// Nếu dùng empty dependencies thì không bao giờ tính toán lại value mới
-//trả về kết quả của một function
+//callback luôn dc gọi sau khi component mounted
